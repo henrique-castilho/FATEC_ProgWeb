57dfc2d741551f341b791e6e7dde23197d6087d9
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-10-19 21:31:34 -0300

    Resolve alguns ajustes

--- desenvolvimento_front/loja/src/app/busca/busca.component.spec.ts
+++ desenvolvimento_front/loja/src/app/busca/busca.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BuscaComponent } from './busca.component';
 
 describe('BuscaComponent', () => {
--- desenvolvimento_front/loja/src/app/model/cliente.ts
+++ desenvolvimento_front/loja/src/app/model/cliente.ts
@@ -2,11 +2,11 @@ export class Cliente {
     public codigo: number = 0;
     public nome: string = "";
     public email: string = "";
-    public documento: string = "";
+    public senha: string = "";
     public telefone: string = "";
+    public documento: string = "";
     public logradouro: string = "";
     public cep: string = "";
     public cidade: string = "";
-    public complemento: string = "";
-    public senha: string = "";
+    public uf: string = "";   
 }
