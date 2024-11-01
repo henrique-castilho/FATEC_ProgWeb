518bf2dc2cdcf568d64a864a2773a6ffffbfe7e6
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-01 00:50:32 -0300

    Configuração inicial do backend da loja com classes de modelo, controladores e repositórios.

---
+++ desenvolvimento_back/loja/src/main/java/com/fatec/loja/Produto.java
@@ -0,0 +1,57 @@
+package com.fatec.loja;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int codigo;
+    private String nome;
+    private String descritivo;
+    private double valor;
+    private int quantidade;
+    private String keywords;
+
+    public Produto(){}
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescritivo() {
+        return descritivo;
+    }
+    public void setDescritivo(String descritivo) {
+        this.descritivo = descritivo;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public String getKeywords() {
+        return keywords;
+    }
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+}
