18f7c690406fd2631e10a4ac548c9fd2cf108083
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-11 18:39:07 -0300

    Integração com o back-end

--- desenvolvimento_front/loja/src/app/cliente/cliente.component.ts
+++ desenvolvimento_front/loja/src/app/cliente/cliente.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Cliente } from '../model/cliente';
+import { ClienteService } from '../service/cliente.service';
 
 @Component({
   selector: 'app-cliente',
@@ -15,21 +16,65 @@ export class ClienteComponent {
   public mensagem: string = "";
   public obj: Cliente = new Cliente();
 
+  constructor(private service: ClienteService){}
+
   public gravar(){
-    localStorage.setItem("cliente", JSON.stringify(this.obj))
-    this.mensagem = "Cadastro atualizado com sucesso!";
+    this.service.gravar(this.obj).subscribe({
+      next : (data) => {
+        this.mensagem = "Cadastro salvo com sucesso!";
+        this.limpar();
+      },
+      error: (err) => {
+        this.mensagem = "Ocorreu um erro, tente mais tarde!";
+      }
+    })
+  }
+
+  public alterar(){
+    this.service.alterar(this.obj).subscribe({
+      next : (data) => {
+        this.mensagem = "Cadastro atualizado com sucesso!";
+        this.limpar();
+      },
+      error: (err) => {
+        this.mensagem = "Ocorreu um erro, tente mais tarde!";
+      }
+    })
+  }
+
+  public remover(){
+    this.service.remover(this.obj.codigo).subscribe({
+      next : (data) => {
+        this.mensagem = "Cadastro removido com sucesso!";
+        this.limpar();
+      },
+      error: (err) => {
+        this.mensagem = "Ocorreu um erro, tente mais tarde!";
+      }
+    })
   }
 
   public carregar(){
-    let json = localStorage.getItem("cliente");
-    if(json == null){
-      window.location.href="./login"
-    } else {
-      this.obj = JSON.parse(json);
-    }
+    this.service.carregar(this.obj.codigo).subscribe({
+      next : (data) => {
+        if(data==null) {
+          this.mensagem = "Cadastro não encontrado, verifique!";
+          this.limpar();
+
+        } else {
+          this.obj = data;
+          this.mensagem = ""
+        }
+      },
+      error: (err) => {
+        this.mensagem = "Ocorreu um erro, tente mais tarde!";
+        this.limpar();
+      }
+    })
   }
 
-  constructor(){
-    this.carregar();
+  public limpar(){
+    this.obj = new Cliente();
   }
+
 }
